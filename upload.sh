2b95cb308003795f0ddf00366210605e7f462c19
Lopez Benjamin <benjamin-lopez@hotmail.fr>
2021-08-18 15:29:03 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ set -e -u -o pipefail
 readonly DOMAIN=""
 
 if [ -z "$DOMAIN" ]; then
-  echo "Unspecified domain"
+  echo "Unspecified domain" >&2
   exit 1
 fi
 
@@ -16,12 +16,16 @@ else
 fi
 
 for FILE in $FILES; do
-  if [ ! -f "$FILE" ]; then
-    echo "$FILE: Missing or invalid file"
+  if [ "$FILE" != "-" -a ! -f "$FILE" ]; then
+    echo "$FILE: Missing or invalid file" >&2
     exit 1
   fi
 done
 
 for FILE in $FILES; do
-  curl --header 'Accept: text/plain' --data-binary @"$FILE" "$DOMAIN"; echo
+  if [ "$FILE" == "-" ]; then
+    curl --header 'Accept: text/plain' --data-binary @"$FILE" "$DOMAIN"; echo
+  else
+	  curl --header 'Accept: text/plain' --header "X-Filename: $(tr -dc \40-\176 <<< $FILE)" --data-binary @"$FILE" "$DOMAIN"; echo
+  fi
 done
