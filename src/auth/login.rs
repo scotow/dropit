24a4b9578bd17a469fb5c5c56d554b240cb2fd16
scotow <contact@scotow.com>
2022-06-15 21:36:38 +0200

    Add back JSON content-length limiter

--- src/auth/login.rs
+++ src/auth/login.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use axum::extract::ContentLengthLimit;
 use axum::response::IntoResponse;
 use axum::{Extension, Json};
 use hyper::StatusCode;
@@ -36,14 +37,9 @@ impl ApiHeader for LoginResponse {
 pub(super) async fn handler(
     Extension(auth): Extension<Arc<Authenticator>>,
     response_type: ResponseType,
-    Json(req): Json<LoginRequest>,
+    ContentLengthLimit(Json(req)): ContentLengthLimit<Json<LoginRequest>, 2048>,
 ) -> Result<impl IntoResponse, Error> {
-    // TODO: Add size limiter back.
-    // if req.size_hint().upper().unwrap_or(u64::MAX) > 2048 {
-    //     return Ok(json_error(AuthError::AuthProcess)?);
-    // }
-
-    let token = auth.create_session(&req.username, &req.password).await?;
-
-    Ok(response_type.to_api_response(LoginResponse { token }))
+    Ok(response_type.to_api_response(LoginResponse {
+        token: auth.create_session(&req.username, &req.password).await?,
+    }))
 }
