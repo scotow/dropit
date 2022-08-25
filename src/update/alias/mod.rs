cd66e7932d702f888e4c3248773de2787d66f3ed
scotow <contact@scotow.com>
2022-08-25 20:42:43 +0200

    Remove serde_json dep

--- src/update/alias/mod.rs
+++ src/update/alias/mod.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use serde::{ser::SerializeStruct, Serialize, Serializer};
-use serde_json::Map;
 
 use crate::response::{ApiHeader, SingleLine};
 
@@ -21,15 +22,15 @@ impl Serialize for AliasChange {
         S: Serializer,
     {
         let mut state = serializer.serialize_struct("AliasChange", 2)?;
-        let mut aliases = Map::with_capacity(2);
-        let mut links = Map::with_capacity(2);
+        let mut aliases = HashMap::with_capacity(2);
+        let mut links = HashMap::with_capacity(2);
         if let Some((alias, link)) = &self.short {
-            aliases.insert("short".to_owned(), alias.to_owned().into());
-            links.insert("short".to_owned(), link.to_owned().into());
+            aliases.insert("short".to_owned(), alias.to_owned());
+            links.insert("short".to_owned(), link.to_owned());
         }
         if let Some((alias, link)) = &self.long {
-            aliases.insert("long".to_owned(), alias.to_owned().into());
-            links.insert("long".to_owned(), link.to_owned().into());
+            aliases.insert("long".to_owned(), alias.to_owned());
+            links.insert("long".to_owned(), link.to_owned());
         }
         state.serialize_field("alias", &aliases)?;
         state.serialize_field("link", &links)?;
